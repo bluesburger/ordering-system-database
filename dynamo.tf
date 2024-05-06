af4bc8bea31ffb209a248fe2f371ff98f7a4fa06
fernandomqs <fernandopozomqs@gmail.com>
2024-05-06 10:58:20 -0300

    dynamo subrede

--- dynamo.tf
+++ dynamo.tf
@@ -1,11 +1,14 @@
 resource "aws_dynamodb_table" "dynamodb_table" {
   name              = var.project_name
-  hash_key         = "todo_id"
+  hash_key          = "todo_id"
   billing_mode      = "PROVISIONED"
   read_capacity     = var.readCapacity
   write_capacity    = var.writeCapacity
-  vpc_security_group_ids = ["${aws_security_group.sg-rds.id}"]
-  db_subnet_group_name = aws_db_subnet_group.subnet-rds.id
+
+  vpc_configuration {
+    subnet_ids = aws_db_subnet_group.subnet-rds.subnet_ids
+  }
+
   attribute {
     name = "todo_id"
     type = "S"
