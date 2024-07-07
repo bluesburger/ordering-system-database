324d8fa24db7b23e67102a6bb28b9e7d560fd3de
fernandomqs <fernandopozomqs@gmail.com>
2024-07-07 19:41:41 -0300

    liberar porta sg

--- sg.tf
+++ sg.tf
@@ -73,6 +73,13 @@ resource "aws_security_group" "private_subnet_sg" {
   }
 
   ingress {
+    from_port       = 100
+    to_port         = 100
+    protocol        = "tcp"
+    security_groups = [aws_security_group.public_subnet_sg.id]
+  }
+
+  ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
